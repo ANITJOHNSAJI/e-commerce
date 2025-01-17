2b8340d0fd10c53fe99e31faee46c9b480a59f9f
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-01-17 16:07:38 +0530

    files updated

--- epro/eapp/models.py
+++ epro/eapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Formdata(models.Model):
+    feed=models.CharField(max_length=100)
--- epro/eapp/urls.py
+++ epro/eapp/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
-from.import views
+from eapp import views
 
 urlpatterns = [
+     path('',views.userlogin,name='userlogin'),
+  path('signup',views.usersignup,name='usersignup'),
+  path('forgotpassword',views.getusername,name='forgotpassword'),
+  path('verifyotp',views.verifyotp,name='verifyotp'),
+  path('passwordreset',views.passwordreset,name='passwordreset'),
+  path('index',views.index,name='index'),
+  path('logout',views.logoutuser,name="logout")
    
 ]
